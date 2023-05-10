54d0f73c3981508a3a3e2c2f521ff358b286cf07
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-10 20:09:03 +0200

    media fix #1

--- app/components/homeCOM/HowWeArr.jsx
+++ app/components/homeCOM/HowWeArr.jsx
@@ -4,7 +4,7 @@ export default function HowWeArr() {
   return (
     <div className='flex flex-col w-full justify-center  items-center border-t-2 border-teal-500 pb-5' >
     <h1 className='mb-2 p-2 bg-teal-500 rounded-b-2xl text-white text-lg' > من نحن </h1>
-    <div className='w-3/4  border-2 border-black rounded  p-10 text-center ' >
+    <div className='w-11/12  border-2 border-black rounded  p-10 text-center ' >
     <p>نحن فريق سترادا 20 ونحن هنا لكي نقدم لك اجود الخامات واجدد الموديلات باقل سعر وباسرع فترة توصيل ونسعي ان نكون من اكبر الشركات العالمية بداية من مصر .</p>
     </div>
     </div>
