b58e6a3ea44f8db9f0d7d9f731bee8b9163f85ca
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-12 21:05:56 +0300

    env 3 fix

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,6 +1,5 @@
 import prisma from "@/lib/prisma";
 import * as bcrypt from 'bcrypt'
-import { json } from "stream/consumers";
 interface requestBody {
   username: string;
   password: string;
@@ -8,14 +7,14 @@ interface requestBody {
 export async function POST(request: Request) {
 
     const body : requestBody = await request.json()
-    console.log(body);
+
     
     const user = await prisma.user.findFirst({
         where:{
             email:body.username
         }
     })
-
+    console.log(user);
     if (user && (await bcrypt.compare(body.password , user.password))) {
         const {password , ...userWithawitpassword} = user
         return new Response(JSON.stringify(userWithawitpassword))
