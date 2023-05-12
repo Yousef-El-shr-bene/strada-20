45479ee3e93133cd403f7c273fc620bb71cf07c3
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-12 20:44:03 +0300

    env 2 fix

--- prisma/migrations/20230512174128_init/migration.sql
+++ prisma/migrations/20230512174128_init/migration.sql
@@ -11,7 +11,6 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Post" (
     "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "authorId" INTEGER NOT NULL,
