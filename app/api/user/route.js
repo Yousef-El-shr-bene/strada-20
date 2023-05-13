3a06e413f6157af372a5c9a444c870b82550628e
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-13 15:02:29 +0300

    env 12 fix

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,12 +1,7 @@
 import prisma from "@/lib/prisma";
 import * as bcrypt from "bcrypt";
-interface requestBody {
-  name: string;
-  password: string;
-  email: string;
-}
 export async function POST(request: Request) {
-  const body: requestBody = await request.json();
+  const body = await request.json();
 
   const user = await prisma.user.create({
     data: {
