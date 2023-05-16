3335c25e09b6028b27c9ae122aad510a5d9fbeda
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-17 01:04:40 +0300

    server fix 15

--- app/page.tsx
+++ app/page.tsx
@@ -8,26 +8,24 @@ import Newcrds from "./components/homeCOM/Newcrds";
 import  HowWeArr  from "./components/homeCOM/HowWeArr";
 import  Rewards  from "./components/homeCOM/Rewards";
 import  CustomLogo  from "./components/homeCOM/CustomLogo";
+import getnewst from "../lib/getnewst"
 
 
 const vazirmatn = Vazirmatn({
  subsets: ['arabic'],
 })
 
-// export async function getStaticProps() {
-//   const data = await fetch(`${process.env.NEXTAUTH_URL}/api/newst`)
-//   const {imgdata} = await data.json()
-//   return { props : { imgdata },fallback: false };
-// }
+// 
 
-export default function Home(/*{imgdata}*/) {
+export default async function Home() {
+  const imgdata = await getnewst()
 
   return (
     <main dir="rtl" className={`flex-col justify-center`}>
         <HomeAdd/>
         <ShopNow/>
         <BigAdd/>
-        {/* <Newcrds data={imgdata} /> */}
+        <Newcrds data={imgdata} />
         <HowWeArr/>
         <Rewards/>
         <CustomLogo/>
--- app/store/page.jsx
+++ app/store/page.jsx
@@ -1,21 +1,17 @@
 import Card from '../components/homeCOM/component/Card'
 import Filter from '../components/storeCOM/filter'
+import getnewst from "../../lib/getnewst"
 
-// export async function getStaticProps() {
-//   const data = await fetch(`${process.env.NEXTAUTH_URL}/api/newst`)
-//   const {imgdata} = await data.json()
-//   return { props : { imgdata },fallback: false };
-// }
 
-export default  function Home(/*{imgdata}*/) {
-
+export default async  function Home() {
+  const imgdata = await getnewst()
 
   return (
 <>
     <div className='flex flex-col' >
     {/* <Filter/> */}
     <div className='flex flex-wrap justify-center items-center' >
-   {/* {imgdata && imgdata.map((e ,i )=>(<><Card id={e.id} text={e.text} url={e.url}  key={i} /></>))} */}
+   {imgdata && imgdata.map((e ,i )=>(<><Card id={e.id} text={e.text} url={e.url}  key={i} /></>))}
     </div>
     </div>
 </>
