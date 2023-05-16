dbd8120b0fb4e5e48444114f28cefc965d39d332
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-16 22:21:04 +0300

    server fix 2

--- app/components/homeCOM/Newcrds.jsx
+++ app/components/homeCOM/Newcrds.jsx
@@ -8,7 +8,7 @@ export default function Newcrds({data}) {
     <div className='flex flex-col w-full bg-slate-800 justify-center items-center border-t-2 border-teal-500 ' >
         <h1 className='mb-2 p-2 bg-teal-500 rounded-b-2xl text-white text-lg' > جديد </h1>
         <div className='flex justify-center flex-wrap m-3 ' >
-            {data && data.map((e,i)=>(<><Card id={e.id} text={e.text} url={e.url} key={i}/></>))}
+            {data && data.map((e)=>(<><Card id={e.id} text={e.text} url={e.url} key={e.id}/></>))}
         </div> 
     </div>
   ) 
