f2011a71ac0eddf74b93ed7488c835634773fdd5
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-17 19:01:24 +0300

    server fix 28

--- app/components/homeCOM/component/Card.jsx
+++ app/components/homeCOM/component/Card.jsx
@@ -1,22 +1,21 @@
-import Image from "next/image"
-import Link from "next/link"
-export default function Card({id,text,url}) {
+import Image from "next/image";
+import Link from "next/link";
+export default function Card({ id, text, url }) {
   console.log(id);
-    return (
-      <Link href={`/store/${id}`} >
-      <div className='flex flex-col border-black border-2 border-spacing-2 w-40 h-52 sm:w-64 sm:h-80 rounded p-3 m-1 bg-teal-50 hover:text-lg ' >
+  return (
+    <Link href={`/store/${id}`}>
+      <div className="flex flex-col border-black border-2 border-spacing-2 w-40 h-52 sm:w-64 sm:h-80 rounded p-3 m-1 bg-teal-50 hover:text-lg ">
         <Image
-        className="h-64 w-96 "
-        src={url} 
-        width={1000}
-        height={1100}
-        alt="img"
-      />
-      <div className="border-t-2 p-1 font-bold text-xs sm:text-base border-black text-center " >
-      <h1>{text}</h1>
+          className="h-64 w-96 "
+          src={url}
+          width={1000}
+          height={1100}
+          alt="img"
+        />
+        <div className="border-t-2 p-1 font-bold text-xs sm:text-base border-black text-center ">
+          <h1>{text}</h1>
+        </div>
       </div>
-      </div>
-      </Link>
-    )
-  } 
-  
+    </Link>
+  );
+}
