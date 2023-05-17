caf085c02f782c1ad694497bf311c2661c851b68
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-17 15:36:02 +0300

    server fix 21

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-// export { default } from "next-auth/middleware"
-// export const config = {
-//     matcher : "/cart/:path*"
-// }
+export { default } from "next-auth/middleware"
+export const config = {
+    matcher : "/cart/:path*"
+}
