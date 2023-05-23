eaacceeb749aa6a43358d0e75b471242a4b71129
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-23 20:02:26 +0300

    sync 1

---
+++ lib/jwt.ts
@@ -0,0 +1,14 @@
+// import jwt,{ JwtPayload } from "jsonwebtoken"
+
+// interface signOpt {
+//     expiresin?:string | number
+// }
+
+// const DEFAULT_SIGN_OPTION :signOpt = {
+//     expiresin : "1h"
+// }
+
+// export default function signinWithJWT(payload : JwtPayload,options : signOpt) {
+//   const secret_key  = process.env.SECRET_KEY;
+//   const token = jwt.sign(payload,secret_key)
+// } 
