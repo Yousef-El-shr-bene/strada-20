259cf62fbcd4f25088eda5e22844fb42e65ddcdb
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-06-04 04:37:17 +0300

    new update #2.1

--- app/store/[crdinfo]/imgenav.jsx
+++ app/store/[crdinfo]/imgenav.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import Order from "./order";
@@ -10,7 +10,7 @@ export default function Imgenav({ onecrd }) {
     crunt: 1,
   });
  const [maindata,usemaindata] = useState({id : onecrd[0].id , size : onecrd[0].size , color  : onecrd[0].color})
-  return (
+  return  (
     <>
           <div className="flex flex-col sm:flex-row justify-center items-center m-3">
         <div className="w-10/12 sm:w-1/2 h-96 m-2 p-2 flex flex-col justify-center items-center rounded border-2 border-black border-solid bg-slate-200">
@@ -49,12 +49,12 @@ export default function Imgenav({ onecrd }) {
           </div>
           <>
             <div className="flex justify-center items-center">
-              {/* <div className="m-1.5 p-1.5 rounded bg-black"></div> */}
               {onecrd[0].color.map((e, i) => (
                 <div
                   key={i}
                   className={ `rounded ${maindata.color === e ? "m-2 p-3" : "m-1 p-3"} `}
                   style={{ backgroundColor: `#${e}` }}
+                  onClick={ ()=> {usemaindata({...maindata,color : e})}}
                 ></div>
               ))}
             </div>
@@ -63,13 +63,13 @@ export default function Imgenav({ onecrd }) {
                 return <div
                 key={i}
               className={`m-1 p-1 h-2 w-2 rounded border-2 border-black border-solid  ${maindata.size === e ? "bg-black border-white text-white border-4" : "bg-white border-white text-white border-2 " } `}
-                 onClick={usemaindata({...maindata,size : e})}
+                 onClick={ ()=> {usemaindata({...maindata,size : e})}}
               >
                 {e}
               </div>
                })}
             </div>
-          </>
+         </>
         </div>
         <Order id={onecrd[0].id} maindata={maindata}  />
         </div>
