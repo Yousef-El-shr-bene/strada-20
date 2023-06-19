a94ed5b3cd403c1e6520b3b7913c6f403f566e22
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-06-19 08:06:13 +0300

    new update #3.2

--- app/api/getallcrds/route.ts
+++ app/api/getallcrds/route.ts
@@ -3,16 +3,10 @@ import { Jwt } from "jsonwebtoken";
 export async function POST(request : Request ) {
     let data 
     const response = await request.json()
-    const skipandtack = Number(response.skipandtack) * 5
+    const skipandtack = Number(response.skipandtack) * 6
     const token = response.token
-    if (skipandtack === 1 * 5) {
-        const imgdata = await prisma.imgdata.findMany({ take : 5})
-        data = imgdata
-    } else {
-        const imgdata = await prisma.imgdata.findMany({skip : skipandtack , take : 5})
-        data = imgdata
-    }
-    console.log(data,"serverrrrrrrrrrrr");
-    
-        return new Response(JSON.stringify({imgdataq : data}))
+
+        const imgdata = await prisma.imgdata.findMany({skip : skipandtack , take : 6})
+
+        return new Response(JSON.stringify({imgdataq : imgdata}))
 }
